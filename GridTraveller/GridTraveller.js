69371c4718a51700c8beecc147f64138d91bb5c9
Shivang Nagta <141389245+ShivangNagta@users.noreply.github.com>
2024-05-24 13:23:00 +0530

    Delete GridTraveller directory

--- GridTraveller/GridTraveller.js
+++
@@ -1,9 +0,0 @@
-const GridTraveller = (n,m) => 
-{
-    if (n === 1 && m === 1) return 1
-    if (n===0 || m===0) return 0
-    return GridTraveller(n-1,m) + GridTraveller(n,m-1)
-}
-
-console.log(GridTraveller(1,1)) 
-console.log(GridTraveller(2,3))
